7b95b3519a015064f06534a2b4592e7530b1446a
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2024-04-21 14:30:21 -0300

    build: atualizando pacote de alarme

---
+++ Módulo 1 - Gerador de Pacotes/wfa_STR/Form1.cs
@@ -0,0 +1,273 @@
+﻿using ScottPlot;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ScottPlot.Drawing.Colormaps;
+
+namespace STR_Gerador
+{
+    public partial class Form1 : Form
+    {
+        Thread threadRecebimentosPacotes = null;
+        Thread[] listaThreadsUnidadesGeradoras;
+        UnidadeGeradoraDadosMedicao[] listaUnidadesGeradorasDadosMedicao;
+        UdpClient udpClient = null;
+        IPEndPoint ipConexaoRecebimentoUDP = null;
+        IPEndPoint ipConexaoEnvioUDP = null;
+        private List<int> dadosPlotarGrafico = new List<int>();
+        public Mutex mutex = new Mutex();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false; // possibilita que componentes sejam chamados por threads diferentes                                    
+
+            formsPlotPacotesEnviados.Plot.Title("Dados de corrente referente à unidade geradora 1", true, Color.Black, 12.0f);
+        }
+
+        private void listViewUnidGeradora_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listaUnidadesGeradorasDadosMedicao == null)
+                return;
+
+            int indiceUnidGeradora = 0;
+            if (listViewUnidGeradora.SelectedItems.Count > 0)
+                indiceUnidGeradora = Convert.ToInt16(listViewUnidGeradora.SelectedItems[0].Text);
+            else
+                return;
+
+            for (int i = 0; i < listaUnidadesGeradorasDadosMedicao.Length; i++)
+            {
+                if (listaUnidadesGeradorasDadosMedicao[i].codDestaUnidade == indiceUnidGeradora)
+                {
+                    propertyGridUnidGeradoras.SelectedObject = listaUnidadesGeradorasDadosMedicao[i];
+                    return;
+                }
+            }
+        }
+
+        private void buttonIniciarEnvio_Click(object sender, EventArgs e)
+        {
+            // parte 0: inicializações de interface
+            buttonIniciarEnvio.Enabled = false;
+            propertyGridUnidGeradoras.Enabled = true;
+            buttonPararEnvio.Enabled = true;
+            timerPlotSinaisEnviados.Start(); // para plotar sinais
+            dadosPlotarGrafico.Clear();
+
+            // parte 1: inicia a conexão UDP
+            udpClient = new UdpClient();
+            udpClient.Connect("127.0.0.1", 11000);
+
+
+            // parte 2: gera objetos equivalentes a unidades geradoras
+            int quantidadeUnidGeradoras = listViewUnidGeradora.Items.Count;
+            listaUnidadesGeradorasDadosMedicao = new UnidadeGeradoraDadosMedicao[quantidadeUnidGeradoras];
+            listaThreadsUnidadesGeradoras = new Thread[quantidadeUnidGeradoras];
+
+            int codigo = 0;
+            int freqEnvio = 0;
+            int correnteOriginal = 0;
+            for (int i = 0; i < quantidadeUnidGeradoras; i++)
+            {
+                codigo = Convert.ToInt16(listViewUnidGeradora.Items[i].SubItems[0].Text);
+                freqEnvio = Convert.ToInt16(listViewUnidGeradora.Items[i].SubItems[1].Text);
+                correnteOriginal = Convert.ToInt16(listViewUnidGeradora.Items[i].SubItems[2].Text);
+                listaUnidadesGeradorasDadosMedicao[i] = new UnidadeGeradoraDadosMedicao(udpClient, mutex, correnteOriginal, codigo, freqEnvio);
+            }
+
+            // parte 3: gera threads
+            for (int i = 0; i < quantidadeUnidGeradoras; i++)
+            {
+                listaThreadsUnidadesGeradoras[i] = new Thread(new ThreadStart(listaUnidadesGeradorasDadosMedicao[i].EnviaPacotesUDPFrequentemente));
+                listaThreadsUnidadesGeradoras[i].Name = "UnidGeradora" + i.ToString();
+                listaThreadsUnidadesGeradoras[i].Start();
+            }
+        }
+
+        private void buttonPararEnvio_Click(object sender, EventArgs e)
+        {
+            // parte 0: inicializações de interface
+            propertyGridUnidGeradoras.Enabled = false;
+            buttonPararEnvio.Enabled = false;
+            timerPlotSinaisEnviados.Stop(); //para plotar sinais
+            dadosPlotarGrafico.Clear();
+            formsPlotPacotesEnviados.Plot.Clear();
+            buttonIniciarEnvio.Enabled = true;
+
+            // parte 1: para os objetos equivalentes a unidades geradoras            
+            for (int i = 0; i < listaUnidadesGeradorasDadosMedicao.Length; i++)
+            {
+                listaUnidadesGeradorasDadosMedicao[i].pararEnvio = true; // vai forçar as threads pararem
+            }
+            //Thread.Sleep(500);
+            listaUnidadesGeradorasDadosMedicao = null;
+
+            // enviar último pacote zerado
+            string formatoPacote = "{'Ia': " + '0' + " ,'Ib': " + '0' + " ,'Ic': " + '0' + " ,'idDispositivo': " + "-1" + "}";
+            byte[] bytes = Encoding.ASCII.GetBytes(formatoPacote);
+            mutex.WaitOne(); // bloqueia esta região para uma simples thread acessar
+            if (udpClient != null)
+                udpClient.Send(bytes, bytes.Length);
+            mutex.ReleaseMutex();
+
+            // parte 2: fecha a conexão UDP
+            if (udpClient != null) 
+            { 
+                udpClient.Close();
+            }
+            if (threadRecebimentosPacotes != null)
+            { 
+                threadRecebimentosPacotes.Abort();
+            }
+
+        }
+
+        private void buttonLimpar_Click(object sender, EventArgs e)
+        {
+            listViewUnidGeradora.Items.Clear();
+            formsPlotPacotesEnviados.Plot.Clear();
+            numericUpDownCodUnidGen.Value = 1;
+        }
+
+        private void buttonAdicionar_Click(object sender, EventArgs e)
+        {
+            listViewUnidGeradora.Items.Add(new ListViewItem(new String[] { numericUpDownCodUnidGen.Value.ToString(), numericUpDownFreqEnvio.Value.ToString(), numericUpDownValorCorrente.Value.ToString() }));
+            numericUpDownCodUnidGen.Value = numericUpDownCodUnidGen.Value + 1;
+        }
+
+        private void timerPlotSinaisEnviados_Tick(object sender, EventArgs e)
+        {
+            if (dadosPlotarGrafico.Count > 300) // se tiver muitas amostras, zera
+            {
+                dadosPlotarGrafico.Clear();
+            }
+            else
+            {
+                dadosPlotarGrafico.Add(Convert.ToInt32(listaUnidadesGeradorasDadosMedicao[0].valorCorrente));
+            }
+
+            // atualiza visualização do gráfico
+            double[] ys = new double[dadosPlotarGrafico.Count];
+            double[] xs = DataGen.Consecutive(dadosPlotarGrafico.Count);
+            for (int i = 0; i < dadosPlotarGrafico.Count; i++)
+            {
+                ys[i] = dadosPlotarGrafico[i];
+            }
+            formsPlotPacotesEnviados.Plot.Clear();
+            if (dadosPlotarGrafico.Count > 1)
+            {
+                formsPlotPacotesEnviados.Plot.AddScatterLines(xs, ys, Color.Blue, 2);
+                formsPlotPacotesEnviados.Refresh();
+            }
+        }
+    }
+
+    public class JSON_DADOS_RECEBIDOS_CORRENTE
+    {
+        public int Ia { get; set; }
+        public int Ib { get; set; }
+        public int Ic { get; set; }
+        public int numPacote { get; set; }
+        public int idDispositivo { get; set; }
+    }
+
+    public class UnidadeGeradoraDadosMedicao
+    {
+        [Browsable(true)]
+        [ReadOnly(false)]
+        [Description("Valor de corrente emitido pelo pacote")]
+        [DisplayName("Corrente normal")]
+        public int valorCorrente { get; set; }
+
+        [Browsable(true)]
+        [ReadOnly(false)]
+        [Description("Código desta unidade geradora de pacotes")]
+        [DisplayName("COD")]
+        public int codDestaUnidade { get; set; }
+
+        [Browsable(true)]
+        [ReadOnly(false)]
+        [Description("Frequencia de envio de pacotes em ms")]
+        [DisplayName("Freq. envio")]
+        public int freqEnvioPacotesMS { get; set; }
+
+        [Browsable(true)]
+        [ReadOnly(true)]
+        [Description("Quantidade de pacotes enviados")]
+        [DisplayName("Pacotes enviados")]
+        public int contadorPacotesEnviados { get; set; }
+
+        [Browsable(false)]
+        public bool pararEnvio { get; set; }
+
+        public Mutex mutex;
+        public UdpClient udpClient;
+        public IPEndPoint ipConexaoEnvioUDP;
+
+        public UnidadeGeradoraDadosMedicao(UdpClient p_udpClient, Mutex p_mutex, int p_valorCorrente, int p_codDestaUnidade, int p_freqEnvioPacotesMS)
+        {
+            mutex = p_mutex;
+            valorCorrente = p_valorCorrente;
+            codDestaUnidade = p_codDestaUnidade;
+            freqEnvioPacotesMS = p_freqEnvioPacotesMS;
+            udpClient = p_udpClient;
+            contadorPacotesEnviados = 0;
+            pararEnvio = false;
+        }
+
+        // Usado como thread
+        public void EnviaPacotesUDPFrequentemente() 
+        {
+            string formatoPacote;
+            string corrente;
+            byte[] bytes;
+            int correntePrev = 0;
+
+            // Caso a corrente não tenha alteração em relação à última, não envia pacotes
+            while (true)
+            {
+                if (valorCorrente != correntePrev)
+                {
+                    if (pararEnvio)
+                        return;
+                        
+                    corrente = Convert.ToString(valorCorrente);
+                    correntePrev = valorCorrente;
+
+                    if (contadorPacotesEnviados < 65000)
+                        contadorPacotesEnviados++;
+                    else
+                        contadorPacotesEnviados = 0;
+                    formatoPacote = "{'Ia': " + corrente +
+                                    " ,'Ib': " + corrente +
+                                    " ,'Ic': " + corrente +
+                                    " ,'numPacote': " + contadorPacotesEnviados.ToString() +
+                                    " ,'idDispositivo': " + codDestaUnidade.ToString() + "}";
+
+                    bytes = Encoding.ASCII.GetBytes(formatoPacote);
+                    mutex.WaitOne(); // Bloqueia esta região para uma simples thread acessar
+                    if (udpClient != null)
+                        udpClient.Send(bytes, bytes.Length);
+                    mutex.ReleaseMutex(); // Desbloqueia esta região
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Módulo 2 - Identificador de Curto/wfa_STR_ProjetoFinal/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace STR_Identificador_Curto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.formsPlotPacotesRecebidos = new ScottPlot.FormsPlot();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listViewDispositivos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonParar = new System.Windows.Forms.Button();
+            this.buttonIniciar = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxConexao = new System.Windows.Forms.ToolStripTextBox();
+            this.timerPlotPacotesRecebidos = new System.Windows.Forms.Timer(this.components);
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(758, 415);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Módulo 2: Identificador de Curto-Circuito IEC";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.formsPlotPacotesRecebidos);
+            this.groupBox3.Location = new System.Drawing.Point(267, 17);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox3.Size = new System.Drawing.Size(480, 388);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Visualização";
+            // 
+            // formsPlotPacotesRecebidos
+            // 
+            this.formsPlotPacotesRecebidos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlotPacotesRecebidos.Location = new System.Drawing.Point(2, 15);
+            this.formsPlotPacotesRecebidos.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
+            this.formsPlotPacotesRecebidos.Name = "formsPlotPacotesRecebidos";
+            this.formsPlotPacotesRecebidos.Size = new System.Drawing.Size(476, 371);
+            this.formsPlotPacotesRecebidos.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listViewDispositivos);
+            this.groupBox2.Controls.Add(this.buttonParar);
+            this.groupBox2.Controls.Add(this.buttonIniciar);
+            this.groupBox2.Controls.Add(this.toolStrip1);
+            this.groupBox2.Location = new System.Drawing.Point(9, 17);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(254, 388);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controle";
+            // 
+            // listViewDispositivos
+            // 
+            this.listViewDispositivos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listViewDispositivos.HideSelection = false;
+            this.listViewDispositivos.Location = new System.Drawing.Point(5, 18);
+            this.listViewDispositivos.Name = "listViewDispositivos";
+            this.listViewDispositivos.Size = new System.Drawing.Size(244, 284);
+            this.listViewDispositivos.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listViewDispositivos.TabIndex = 5;
+            this.listViewDispositivos.UseCompatibleStateImageBehavior = false;
+            this.listViewDispositivos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Corrente (A)";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 80;
+            // 
+            // buttonParar
+            // 
+            this.buttonParar.Location = new System.Drawing.Point(165, 307);
+            this.buttonParar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonParar.Name = "buttonParar";
+            this.buttonParar.Size = new System.Drawing.Size(70, 22);
+            this.buttonParar.TabIndex = 4;
+            this.buttonParar.Text = "Parar";
+            this.buttonParar.UseVisualStyleBackColor = true;
+            this.buttonParar.Click += new System.EventHandler(this.buttonParar_Click);
+            // 
+            // buttonIniciar
+            // 
+            this.buttonIniciar.Location = new System.Drawing.Point(16, 307);
+            this.buttonIniciar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonIniciar.Name = "buttonIniciar";
+            this.buttonIniciar.Size = new System.Drawing.Size(70, 22);
+            this.buttonIniciar.TabIndex = 3;
+            this.buttonIniciar.Text = "Iniciar";
+            this.buttonIniciar.UseVisualStyleBackColor = true;
+            this.buttonIniciar.Click += new System.EventHandler(this.buttonIniciar_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBoxConexao});
+            this.toolStrip1.Location = new System.Drawing.Point(2, 359);
+            this.toolStrip1.Margin = new System.Windows.Forms.Padding(4);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(2);
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(250, 27);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(60, 20);
+            this.toolStripLabel1.Text = "Conexão: ";
+            // 
+            // toolStripTextBoxConexao
+            // 
+            this.toolStripTextBoxConexao.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBoxConexao.Name = "toolStripTextBoxConexao";
+            this.toolStripTextBoxConexao.ReadOnly = true;
+            this.toolStripTextBoxConexao.Size = new System.Drawing.Size(100, 23);
+            // 
+            // timerPlotPacotesRecebidos
+            // 
+            this.timerPlotPacotesRecebidos.Interval = 500;
+            this.timerPlotPacotesRecebidos.Tick += new System.EventHandler(this.timerPlotPacotesRecebidos_Tick);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Tempo de Atuação (s)";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 117;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Identificador de Curto";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private ScottPlot.FormsPlot formsPlotPacotesRecebidos;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Button buttonParar;
+        private System.Windows.Forms.Button buttonIniciar;
+        private System.Windows.Forms.Timer timerPlotPacotesRecebidos;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxConexao;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ListView listViewDispositivos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
