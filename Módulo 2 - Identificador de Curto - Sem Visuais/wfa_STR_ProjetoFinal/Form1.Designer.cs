1123892d1ec0ce05eba57b8cc9c6a50a83d6810e
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2024-04-22 16:13:06 -0300

    build: implementando módulo sem visuais

---
+++ Módulo 2 - Identificador de Curto - Sem Visuais/wfa_STR_ProjetoFinal/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace STR_Identificador_Curto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listViewDispositivos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonParar = new System.Windows.Forms.Button();
+            this.buttonIniciar = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripTextBoxConexao = new System.Windows.Forms.ToolStripTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(274, 415);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Módulo 2: Identificador de Curto-Circuito IEC";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listViewDispositivos);
+            this.groupBox2.Controls.Add(this.buttonParar);
+            this.groupBox2.Controls.Add(this.buttonIniciar);
+            this.groupBox2.Controls.Add(this.toolStrip1);
+            this.groupBox2.Location = new System.Drawing.Point(9, 17);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(254, 388);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controle";
+            // 
+            // listViewDispositivos
+            // 
+            this.listViewDispositivos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listViewDispositivos.HideSelection = false;
+            this.listViewDispositivos.Location = new System.Drawing.Point(5, 18);
+            this.listViewDispositivos.Name = "listViewDispositivos";
+            this.listViewDispositivos.Size = new System.Drawing.Size(244, 284);
+            this.listViewDispositivos.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listViewDispositivos.TabIndex = 5;
+            this.listViewDispositivos.UseCompatibleStateImageBehavior = false;
+            this.listViewDispositivos.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            this.columnHeader1.Width = 40;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Corrente (A)";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 80;
+            // 
+            // buttonParar
+            // 
+            this.buttonParar.Location = new System.Drawing.Point(165, 307);
+            this.buttonParar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonParar.Name = "buttonParar";
+            this.buttonParar.Size = new System.Drawing.Size(70, 22);
+            this.buttonParar.TabIndex = 4;
+            this.buttonParar.Text = "Parar";
+            this.buttonParar.UseVisualStyleBackColor = true;
+            this.buttonParar.Click += new System.EventHandler(this.buttonParar_Click);
+            // 
+            // buttonIniciar
+            // 
+            this.buttonIniciar.Location = new System.Drawing.Point(16, 307);
+            this.buttonIniciar.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonIniciar.Name = "buttonIniciar";
+            this.buttonIniciar.Size = new System.Drawing.Size(70, 22);
+            this.buttonIniciar.TabIndex = 3;
+            this.buttonIniciar.Text = "Iniciar";
+            this.buttonIniciar.UseVisualStyleBackColor = true;
+            this.buttonIniciar.Click += new System.EventHandler(this.buttonIniciar_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripTextBoxConexao});
+            this.toolStrip1.Location = new System.Drawing.Point(2, 359);
+            this.toolStrip1.Margin = new System.Windows.Forms.Padding(4);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Padding = new System.Windows.Forms.Padding(2);
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(250, 27);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(60, 20);
+            this.toolStripLabel1.Text = "Conexão: ";
+            // 
+            // toolStripTextBoxConexao
+            // 
+            this.toolStripTextBoxConexao.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBoxConexao.Name = "toolStripTextBoxConexao";
+            this.toolStripTextBoxConexao.ReadOnly = true;
+            this.toolStripTextBoxConexao.Size = new System.Drawing.Size(100, 23);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 415);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Identificador de Curto";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Button buttonParar;
+        private System.Windows.Forms.Button buttonIniciar;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBoxConexao;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ListView listViewDispositivos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Módulo 2 - Identificador de Curto - Sem Visuais/wfa_STR_ProjetoFinal/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Diagnostics;
+
+namespace STR_Identificador_Curto
+{
+    public partial class Form1 : Form
+    {
+        private List<UnidadeMonitoramentoDados> listaDispositivos = new List<UnidadeMonitoramentoDados>();
+        private List<int> dadosPlotarGrafico = new List<int>();
+        public Mutex mutex = new Mutex();
+        Thread threadRecebimentosPacotes = null;
+        JSON_Dados_Corrente dadosRecebidosJSON;
+        UdpClient udpServer = null;
+        IPEndPoint remoteEP = null;
+        string mensagemRecebida;
+        byte[] bytesRecebidos;
+        bool pararRecebimentoDados = false;
+        double correnteMedia = 0;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; 
+            InitializeComponent();
+        }
+
+        private void buttonIniciar_Click(object sender, EventArgs e)
+        {
+            buttonIniciar.Enabled = false;
+            buttonParar.Enabled = true;
+            dadosPlotarGrafico.Clear();
+            pararRecebimentoDados = false;
+
+            udpServer = new UdpClient(11000);
+            remoteEP = new IPEndPoint(IPAddress.Any, 11000);
+
+            toolStripTextBoxConexao.Text = "UDP (" + remoteEP.Address.ToString() + ")";
+
+            // Inicializa a lista de dispositivos
+            listaDispositivos = new List<UnidadeMonitoramentoDados>();
+            for (int i = 0; i <= 5; i++)
+            {
+                listaDispositivos.Add(null);
+            }
+
+            // Inicia o recebimento de pacotes
+            threadRecebimentosPacotes = new Thread(RecebimentoPacotesUDP);
+            threadRecebimentosPacotes.Start();
+        }
+
+        private void buttonParar_Click(object sender, EventArgs e)
+        {
+            buttonParar.Enabled = false;
+            dadosPlotarGrafico.Clear();
+            buttonIniciar.Enabled = true;
+            pararRecebimentoDados = true;
+            listViewDispositivos.Clear();
+
+            // Encerra a conexão UDP
+            if (udpServer != null)
+            {
+                udpServer.Close();
+            }
+
+            // Aguarda o término da thread de recebimento de pacotes
+            if (threadRecebimentosPacotes != null && threadRecebimentosPacotes.IsAlive)
+            {
+                threadRecebimentosPacotes.Join();
+            }
+        }
+
+        List<int> listaIDDispositivos = new List<int>();  
+        private void RecebimentoPacotesUDP()
+        {
+            while (true)
+            {
+                if (pararRecebimentoDados)
+                    return;
+
+                bytesRecebidos = udpServer.Receive(ref remoteEP);
+                mensagemRecebida = Encoding.ASCII.GetString(bytesRecebidos);
+                dadosRecebidosJSON = JsonConvert.DeserializeObject<JSON_Dados_Corrente>(mensagemRecebida);
+
+                int id = dadosRecebidosJSON.idDispositivo;
+                correnteMedia = (dadosRecebidosJSON.Ia + dadosRecebidosJSON.Ib + dadosRecebidosJSON.Ic) / 3;
+
+                // Verifica se não é o pacote de encerramento de envios
+                if (id != -1)
+                {
+                    if (listaDispositivos.Count <= id || listaDispositivos[id] == null)
+                    {
+                        // Cria uma nova instância de UnidadeMonitoramentoDados se ainda não existir
+                        UnidadeMonitoramentoDados dispositivo = new UnidadeMonitoramentoDados(id, correnteMedia, mutex);
+                        listaDispositivos[id] = dispositivo;
+                        Thread threadDispositivo = new Thread(() => dispositivo.AnalisaDados());
+                        threadDispositivo.Start();
+                    }
+                    else
+                    {
+                        // Atualiza os dados da instância existente de UnidadeMonitoramentoDados
+                        listaDispositivos[id].AtualizaCorrenteMedia(correnteMedia);
+                    }
+
+                    if (listaIDDispositivos.Contains(id))
+                    {
+                        // Atualiza listview com novos valores de corrente
+                        foreach (ListViewItem item in listViewDispositivos.Items)
+                        {
+                            if (item.SubItems[0].Text == id.ToString()) // Procura pelo ID
+                            {
+                                item.SubItems[1].Text = correnteMedia.ToString(); // Atualiza a corrente
+                            }
+                        }
+                    }
+                    else
+                    {
+                        listViewDispositivos.Items.Add(new ListViewItem(new String[] { id.ToString(), correnteMedia.ToString(), "-" }));
+                        listaIDDispositivos.Add(id);
+                    }
+                }
+                else
+                {
+                    // Atualiza todas as correntes para 0
+                    for (int i = 1; i < listaDispositivos.Count; i++)
+                    {
+                        if (listaDispositivos[i] != null)  
+                            listaDispositivos[i].AtualizaCorrenteMedia(0);
+                    }
+                }
+            }
+        }
+    } // -------- FIM CLASSE ---------
+
+
+    public class JSON_Dados_Corrente
+    {
+        public int Ia { get; set; }
+        public int Ib { get; set; }
+        public int Ic { get; set; }
+        public int numPacote { get; set; }
+        public int idDispositivo { get; set; }
+    }
+
+    public class UnidadeMonitoramentoDados
+    {
+        private int idDispositivo;
+        private double dial = 0.25;
+        private double correntePartida51 = 600; // A corrente nominal para esse caso seria 502A. A aproximação feita foi I51 = In x 1,2.
+        private double correnteCCMax = 7320;    // A corrente calculada no vídeo-base https://www.youtube.com/watch?v=rGn6zAh6ce4
+        private double tempoAtuacao;
+        private double correnteMedia;
+        private System.Threading.Timer timer;
+        private double tempoRestanteEmSegundos;
+        private Mutex mutex;
+        private UdpClient udpClient;
+        Thread threadAlarme = null;
+        Thread threadTempoAtuacao = null;
+        double correnteSendoAnalisada = 0;
+        bool timerComecou = false;
+        bool emCurto = false;
+        int contadorPacotes = 0;
+
+        public UnidadeMonitoramentoDados(int p_id, double p_correnteMedia, Mutex p_mutex)
+        {
+            this.idDispositivo = p_id;
+            this.correnteMedia = p_correnteMedia;
+            this.mutex = p_mutex;
+            
+            // Inicialize o cliente UDP e defina o endereço de broadcast
+            udpClient = new UdpClient();
+            udpClient.EnableBroadcast = true;
+        }
+
+        public void AnalisaDados()
+        {
+            while (true)
+            {
+                if (correnteMedia < correntePartida51)
+                {
+                    emCurto = false;
+                    tempoRestanteEmSegundos = 0;
+                    if (timerComecou)
+                    {
+                        timer.Dispose();
+                        timerComecou = false;
+                        threadAlarme.Abort();
+                        EnviaPacote(idDispositivo, emCurto, correnteSendoAnalisada, DateTime.Now.ToString("h:mm:ss.fff tt"));
+                    }
+                }
+                else 
+                {
+                    emCurto = true;
+
+                    if (!timerComecou)
+                    {
+                        tempoRestanteEmSegundos = 0;
+                        timer = new System.Threading.Timer(TimerCallback, null, 0, 10);
+
+                        timerComecou = true;
+                        correnteSendoAnalisada = correnteMedia;
+                    }
+                    else if (correnteMedia != correnteSendoAnalisada)
+                    {
+                        // Reinicializa o timer mantendo a contagem do "tempoRestanteEmSegundos" anterior
+                        timer.Dispose();
+                        if (threadAlarme != null)
+                            threadAlarme.Abort();
+
+                        timer = new System.Threading.Timer(TimerCallback, null, 0, 10);
+                        timerComecou = true;
+                        correnteSendoAnalisada = correnteMedia;
+                    }
+                }
+            }
+        }
+
+        private void MostraTempoAtuacao()
+        {
+            MessageBox.Show("Corrente: " + this.correnteMedia + " A / Tempo de Atuação: " + this.tempoAtuacao);
+            threadTempoAtuacao.Abort();
+        }
+        
+        private void TimerCallback(object state)
+        {
+            // Atualiza o tempo restante
+            tempoRestanteEmSegundos += 0.01;
+            tempoAtuacao = dial * (13.5 / ((correnteMedia / correntePartida51) - 1));
+
+            if (tempoRestanteEmSegundos >= tempoAtuacao)
+            {
+                // Para o timer e despacha pacote 99/1 quando o tempo limite for atingido
+                timer.Dispose();
+                threadAlarme = new Thread(new ThreadStart(Alarme));
+                threadAlarme.Start();
+            }
+        }
+
+        public void AtualizaCorrenteMedia(double novaCorrenteMedia)
+        {
+            // Atualiza a corrente média com o novo valor recebido
+            this.correnteMedia = novaCorrenteMedia;
+        }
+
+        public void Alarme()
+        {
+            while (emCurto)
+            {
+                // Suspende a Thread por um segundo para evitar sobrecarregamento da rede
+                if (contadorPacotes > 1000)
+                    Thread.Sleep(1000);
+
+                EnviaPacote(idDispositivo, emCurto, correnteSendoAnalisada, DateTime.Now.ToString("h:mm:ss.fff tt"));
+                contadorPacotes++;
+
+                // Verifica a urgência de enviar pacotes
+                if (correnteMedia < correnteCCMax)
+                {
+                    // Se estourar o limite, para o envio (tempo de espera muito longo)
+                    if (tempoAtuacao * 1000 > Int32.MaxValue)
+                    {
+                        break;
+                    }
+                    else
+                        Thread.Sleep(Convert.ToInt32(tempoAtuacao * 1000));
+                }
+                else
+                {
+                    // Caso a corrente seja maior que o limite superior, envia com mais rapidez
+                    Thread.Sleep(100);
+                }
+            }
+
+            contadorPacotes = 0;
+        }
+        
+        private void EnviaPacote(int idDispositivo, bool emCurto, double corrente, string momento)
+        {
+            // Pacote a ser enviado para o módulo atuador
+            string formatoPacote = "{'ID': " + idDispositivo +
+                                   ", 'Curto': " + emCurto +
+                                   ", 'Corrente': " + corrente +
+                                   ", 'Momento': " + momento + "}";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(formatoPacote);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 11001);
+
+            mutex.WaitOne();
+            try
+            {
+                udpClient.Send(bytes, bytes.Length, endPoint);
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
