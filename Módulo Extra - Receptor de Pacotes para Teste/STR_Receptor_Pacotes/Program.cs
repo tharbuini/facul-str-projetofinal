96185d11c472c4da0b1410aa919ed0b38b0abbeb
Thiago Rodrigues <48720971+tharbuini@users.noreply.github.com>
2024-04-22 15:54:49 -0300

    build: implementando módulo de teste

---
+++ Módulo Extra - Receptor de Pacotes para Teste/STR_Receptor_Pacotes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace STR_Receptor_Pacotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpServer = new UdpClient(11001);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                while (true)
+                {
+                    byte[] bytes = udpServer.Receive(ref endPoint);
+                    string formatoPacote = Encoding.ASCII.GetString(bytes);
+
+                    Console.WriteLine("Mensagem recebida: " + formatoPacote);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpServer.Close();
+            }
+        }
+    }
+}
